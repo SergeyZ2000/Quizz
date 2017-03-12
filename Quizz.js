fe3a5a886daa1afeec3b1a3842dc7f2ec8b87617
buka.funf <buka.funf@gmail.com>
2017-03-12 04:40:11 -0400

    first

---
+++ Quizz.js
@@ -0,0 +1,12 @@
+var Questions = [  
+{ 
+  text: "Что не считается содержимым при box-sizing: border-box?",
+  options: ["padding","margin","border","Все считается содержимым"],
+  correct: 1  
+},  
+{  
+  text: "Каким будет результат вычисления 1 / 0 в JavaScript",
+  options: ["null","undefined","infinity","ошибка"],
+  correct: 2   
+}   
+];
